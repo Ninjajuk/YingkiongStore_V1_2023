61bcfef982864bbcbc0dcc6b658c4bf83724791f
Samsu <biosamsuddin@gmail.com>
2024-02-15 16:45:37 +0530

    lazy

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -1,10 +1,18 @@
-import React, { useState, useEffect, Suspense } from 'react';
+import React, { useState, useEffect, Suspense,lazy } from 'react';
 // import ProducList from './componentsProducts/ProducList'
 import { Circles } from "react-loader-spinner";
 import {getallProducts} from '../../API/productAPI'
 
-const ProducList = React.lazy(() => import('./componentsProducts/ProducList'));
+// const ProducList = React.lazy(() => import('./componentsProducts/ProducList'));
 
+const ProducList = lazy(() => delayForDemo(import('./componentsProducts/ProducList')));
+
+// Add a fixed delay so you can see the loading state
+function delayForDemo(promise) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000);
+  }).then(() => promise);
+}
 
 const Product = () => {
   const [data, setData] = useState([]);
@@ -33,22 +41,20 @@ const Product = () => {
     <>
       <Suspense
         fallback={
-  
-            <div className="flex items-center justify-center h-screen">
-              <Circles
-                height="80"
-                width="80"
-                color="#7b09e7"
-                ariaLabel="circles-loading"
-                wrapperStyle={{}}
-                wrapperClass=""
-                visible={true}
-              />
-            </div>
-
+          <div className="flex items-center justify-center h-screen">
+            <Circles
+              height="80"
+              width="80"
+              color="#7b09e7"
+              ariaLabel="circles-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          </div>
         }
       >
- <ProducList data={data} uniqueCategories={uniqueCategories} />
+        <ProducList data={data} uniqueCategories={uniqueCategories} />
       </Suspense>
     </>
   );
