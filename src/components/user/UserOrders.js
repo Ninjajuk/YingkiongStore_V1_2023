8236c33debbab82334f3e65023b36cdd17c35a4d
Samsu <biosamsuddin@gmail.com>
2024-02-15 15:17:12 +0530

    view order

--- src/components/user/UserOrders.js
+++ src/components/user/UserOrders.js
@@ -164,13 +164,13 @@ export default function OrderHistoryInvoicePanel() {
                         <span>View Order</span>
                         <span className="sr-only">{order.number}</span>
                       </a>
-                      <a
+                      {/* <a
                         href={order.invoiceHref}
                         className="flex items-center justify-center rounded-md border border-gray-300 bg-white py-2 px-2.5 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                       >
                         <span>View Invoice</span>
                         <span className="sr-only">for order {order.number}</span>
-                      </a>
+                      </a> */}
                     </div>
                   </div>
 
