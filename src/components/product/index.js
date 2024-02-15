59ab0b50dcec45ad8e805a1bad5beacf4532d281
Samsu <biosamsuddin@gmail.com>
2024-02-15 16:10:10 +0530

    l

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect, Suspense } from 'react';
 // import ProducList from './componentsProducts/ProducList'
-
+import { Circles } from "react-loader-spinner";
 import {getallProducts} from '../../API/productAPI'
+
 const ProducList = React.lazy(() => import('./componentsProducts/ProducList'));
+
+
 const Product = () => {
   const [data, setData] = useState([]);
   const [uniqueCategories, setUniqueCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function getData() {
@@ -23,16 +27,37 @@ const Product = () => {
 
     getData();
   }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false); // Set loading to false after 2 seconds
+    }, 3000);
+
+    return () => clearTimeout(timer); // Clear the timer on component unmount
+  }, []);
   return (
     <>
-     <Suspense fallback={<div>Loading...</div>}>
-  
-     <ProducList data={data} uniqueCategories={uniqueCategories} />
-     </Suspense>
- 
- 
+      <Suspense
+        fallback={
+          isLoading ? (
+            <div className="flex items-center justify-center h-screen">
+              <Circles
+                height="80"
+                width="80"
+                color="#7b09e7"
+                ariaLabel="circles-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+              />
+            </div>
+          ) : null
+        }
+      >
+       {!isLoading && <ProducList data={data} uniqueCategories={uniqueCategories} />}
+      </Suspense>
     </>
-  )
+  );
 }
 
 export default Product
