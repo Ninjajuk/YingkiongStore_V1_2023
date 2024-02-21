a51460bf9428d6c4bb29ddf88b9af2fd0fe08eda
Samsu <biosamsuddin@gmail.com>
2024-02-21 12:17:53 +0530

    lazy loading finally done

--- src/components/product/TestProduct.js
+++ src/components/product/TestProduct.js
@@ -2,169 +2,127 @@ import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import { Circles } from "react-loader-spinner";
 import TestProducList from "./componentsProducts/TestProductList";
-
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 const TestProduct = () => {
   const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(null);
-  const [index, setIndex] = useState(1);
-
-  const fetchData = useCallback(async () => {
-    if (isLoading) return;
-    setIsLoading(true);
-
-    try {
-      const response = await axios.get(`http://localhost:8000/products?offset=${index}0&limit=12`);
-      const data = response.data.products;
-      console.log(response.data.products,'http://localhost:8000/products?offset=${index *10}&limit=12')
-
-        setItems((prevItems) => [...prevItems, ...data]);
-     
-    } catch (error) {
-      console.error('Error fetching products:', error)
-    } 
-    setIndex((prevIndex) => prevIndex + 1);
-    setIsLoading(false);
-  }, [index,isLoading]);
-
-  const getData = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      const response = await axios.get(
-        `http://localhost:8000/products?offset=1&limit=12`
-      );
-      setItems(prevItems => [...prevItems, ...response.data.products]);
-      setHasMore(response.data.hasMore);
-      console.log('first ', response.data.hasMore);
-    } catch (error) {
-      console.log(error);
-    }
-    setIsLoading(false);
-  }, []); 
+  const [hasMore, setHasMore] = useState(true);
+  const [index, setIndex] = useState(1); 
 
   useEffect(() => {
-    getData();
-  }, [getData]); 
+    axios
+      .get("http://localhost:8000/products?skip=0&limit=12")
+      .then((res) => {
+        setItems(res.data.products)
+        console.log(res.data.products,'First API call')
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const { scrollTop, clientHeight, scrollHeight } =
-  //       document.documentElement;
-  //     if (scrollTop + clientHeight >= scrollHeight - 20) {
-  //       console.log(fetchData());
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, [hasMore]);
-
+  const fetchData = () => {
+    axios
+      .get(`http://localhost:8000/products?skip=${index * 12}&limit=12`)
+      .then((res) => {
+        const newItems = res.data.products;
+        console.log(res.data.products,'2nd API call')
+        console.log(res.data.products.length)
+        setItems((prevItems) => [...prevItems, ...newItems]);
+        res.data.products.length > 0 ? setHasMore(true) : setHasMore(false);
+      })
+      .catch((err) => console.log(err));
+      setIndex((prevIndex) => prevIndex + 1);
+  };
 
   return (
     <>
+ <InfiniteScroll
+      dataLength={items.length}
+      next={fetchData} // Call fetchData function when reaching bottom
+      hasMore={hasMore}
+      loader={        <div className="flex items-center justify-center h-screen">
+      <Circles
+        height="80"
+        width="80"
+        color="#7b09e7"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>}
+    >
+      <div className='container'>
 
-      {
-    <div className='container'>
-    <div className='row'>
-      {items.map((item) => (
-        <TestProducList data={items} key={item.id} />
-      ))}
-    </div>
-    {isLoading && (
-        <div className="flex items-center justify-center h-screen">
-          <Circles
-            height="80"
-            width="80"
-            color="#7b09e7"
-            ariaLabel="circles-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-          />
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
+          {items &&
+            items.map((item) => <>
+              <div
+            
+                className=" group relative px-2 py-2 shadow-md rounded-md flex flex-col justify-between"
+              >
+                {/* <Link to={`/shop/${product.id}`}> */}
+                <a href={`/shop/${item.id}`}>
+                  <div className="h-40 w-40 aspect-h-1 aspect-w-1  overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-48">
+                    <img
+                      src={item.thumbnail}
+                      alt={item.title}
+                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                    />
+                  </div>
+                </a>
+
+                <div className="mt-4 flex flex-col">
+                  <a href={`/shop/${item.id}`}>
+                    <p className=" font-medium text-gray-900">
+                      {item.title}
+                    </p>
+                  </a>
+                  <p className="text-sm font-medium text-gray-900 flex items-center justify-between">
+                    <span className="font-semibold text-lg">
+                      ₹{item.price}
+                    </span>
+                    <span className="text-green-500">
+                      {item.discountPercentage}% off
+                    </span>
+                  </p>
+                  <div className="py-2">
+                    {/* {product.category in categoryOptionsMap && (
+                      <select className="block w-full p-2 border border-gray-300 rounded-md mt-1">
+                        {categoryOptionsMap[product.category].map(
+                          (weight, weightIndex) => (
+                            <option key={weightIndex} value={weight}>
+                              {weight} kg
+                            </option>
+                          )
+                        )}
+                      </select>
+                    )} */}
+                  </div>
+                </div>
+
+                <div className="text-center py-3 mb-3">
+                  <button
+                    // onClick={() => handleAddToCart(product)}
+                    className={`w-full text-white bg-purple-700  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+                  >
+                    <span className={`inline-block w-18 `}>
+                      {/* {isItemInCart(product._id, cartItems)
+                        ? "Remove"
+                        : "Add to Cart"} */}
+                      Add
+                    </span>
+                  </button>
+                </div>
+                {/* </Link> */}
+              </div>
+            </>)}
         </div>
-      )}
-  </div>
-      }
-
-{!hasMore && items.length > 0 && <div>No more products to load</div>}
+      </div>
+    </InfiniteScroll>
     </>
   );
 }
 
 export default TestProduct
 
-// import React, { useState, useEffect, useCallback } from "react";
-// import axios from "axios";
-// import { Circles } from "react-loader-spinner";
-// import TestProducList from "./componentsProducts/TestProductList";
-// import InfiniteScroll from "react-infinite-scroll-component";
-
-
-// const TestProduct = () => {
-//   const [items, setItems] = useState([]);
-//   const [hasMore, setHasMore] = useState(true);
-//   const [index, setIndex] = useState(1);
-
-//   useEffect(() => {
-//     axios
-//       // .get("https://api.escuelajs.co/api/v1/products?offset=10&limit=12")
-//       .get("http://localhost:8000/products?offset=1&limit=12")
-//       .then((res) => {setItems(res.data.products)
-//         console.log(res.data.products)
-//         setIndex((prevIndex) => prevIndex + 1);
-//       })
-//       .catch((err) => console.log(err));
-//   }, []);
-
-//   const fetchMoreData = () => {
-//     axios
-
-//       .get(`http://localhost:8000/products?offset=${index}0&limit=12`)
-//       .then((res) => {
-//         setItems((prevItems) => [...prevItems, ...res.data.products]);
-//         console.log( res.data.products.length )
-
-//         res.data.products.length > 0 ? setHasMore(true) : setHasMore(false);
-//       })
-//       .catch((err) => console.log(err));
-
-//     setIndex((prevIndex) => prevIndex + 1);
-//   };
-
-
-
-//   return (
-//     <>
-
-//     <InfiniteScroll
-//       dataLength={items.length}
-//       next={fetchMoreData}
-//       hasMore={hasMore}
-//       loader={       <Circles
-//         height="80"
-//         width="80"
-//         color="#7b09e7"
-//         ariaLabel="circles-loading"
-//         wrapperStyle={{}}
-//         wrapperClass=""
-//         visible={true}
-//       />}
-//     >
-//       <div className='container'>
-//         <div className='row'>
-//         {items.map((item) => (
-//         <TestProducList data={items} key={item.id} />
-//       ))}
-//       {!hasMore && items.length > 0 && <div>No more products to load</div>}
-//         </div>
-//       </div>
-//     </InfiniteScroll>
-//     </>
-//   );
-// }
-
-// export default TestProduct
--- src/pages/Test1.js
+++ src/pages/Test1.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
-
+import { Circles } from "react-loader-spinner";
 
 const InfiniteScrollExample1 = () => {
   const [items, setItems] = useState([]);
   const [hasMore, setHasMore] = useState(true);
-  const [index, setIndex] = useState(2);
+  const [index, setIndex] = useState(1);
 
   useEffect(() => {
     axios
-      .get("https://api.escuelajs.co/api/v1/products?offset=10&limit=12")
+      .get("https://api.escuelajs.co/api/v1/products?offset=0&limit=12")
       .then((res) => setItems(res.data))
       .catch((err) => console.log(err));
   }, []);
@@ -33,14 +33,82 @@ const InfiniteScrollExample1 = () => {
       dataLength={items.length}
       next={fetchMoreData}
       hasMore={hasMore}
-      loader={<h1>Loading.....</h1>}
+      loader={        <div className="flex items-center justify-center h-screen">
+      <Circles
+        height="80"
+        width="80"
+        color="#7b09e7"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>}
     >
       <div className='container'>
-        <div className='row'>
+
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
           {items &&
             items.map((item) => <>
-            <h1>{item.tile}</h1>
-            <img src={item.images[0]}/>
+              <div
+            
+                className=" group relative px-2 py-2 shadow-md rounded-md flex flex-col justify-between"
+              >
+                {/* <Link to={`/shop/${product.id}`}> */}
+                <a href={`/shop/${item.id}`}>
+                  <div className="h-40 w-40 aspect-h-1 aspect-w-1  overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-48">
+                    <img
+                      src={item.images[0]}
+                      alt={item.title}
+                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                    />
+                  </div>
+                </a>
+
+                <div className="mt-4 flex flex-col">
+                  <a href={`/shop/${item.id}`}>
+                    <p className=" font-medium text-gray-900">
+                      {item.title}
+                    </p>
+                  </a>
+                  <p className="text-sm font-medium text-gray-900 flex items-center justify-between">
+                    <span className="font-semibold text-lg">
+                      ₹{item.price}
+                    </span>
+                    <span className="text-green-500">
+                      {item.discountPercentage}% off
+                    </span>
+                  </p>
+                  <div className="py-2">
+                    {/* {product.category in categoryOptionsMap && (
+                      <select className="block w-full p-2 border border-gray-300 rounded-md mt-1">
+                        {categoryOptionsMap[product.category].map(
+                          (weight, weightIndex) => (
+                            <option key={weightIndex} value={weight}>
+                              {weight} kg
+                            </option>
+                          )
+                        )}
+                      </select>
+                    )} */}
+                  </div>
+                </div>
+
+                <div className="text-center py-3 mb-3">
+                  <button
+                    // onClick={() => handleAddToCart(product)}
+                    className={`w-full text-white bg-purple-700  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+                  >
+                    <span className={`inline-block w-18 `}>
+                      {/* {isItemInCart(product._id, cartItems)
+                        ? "Remove"
+                        : "Add to Cart"} */}
+                      Add
+                    </span>
+                  </button>
+                </div>
+                {/* </Link> */}
+              </div>
             </>)}
         </div>
       </div>
